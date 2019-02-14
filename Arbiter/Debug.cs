87f6a7b60304e2869c5f53d70de34864203a3ca7
ReVampYkt <ilya_ykt@live.ru>
2019-02-14 21:50:25 +0900

    Doing logger

---
+++ Arbiter/Debug.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Arbiter
+{
+    public class Debug
+    {
+        private static Debug instance;
+
+        private FileStream fileStream;
+
+        public static void Initialization()
+        {
+            instance = new Debug();
+
+            string path = Path.Combine(Program.WorkingDirectory, "Log");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string fileName = Path.Combine(path, $"{DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day} {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}.log");
+
+            instance.fileStream = File.Create(fileName);
+        }
+
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+
+        }
+
+        public static void LogError(string message)
+        {
+            Console.WriteLine(message);
+
+        }
+
+        public static void LogWarning(string message)
+        {
+            Console.WriteLine(message);
+
+        }
+    }
+}
--- Arbiter/Program.cs
+++ Arbiter/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Threading;
 
 namespace Arbiter
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static string WorkingDirectory = "";
+
+        private static bool IsCancelled = false;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length > 0)
+                WorkingDirectory = args[0];
+
+            Debug.Initialization();
+
+            if (WorkingDirectory == "")
+            {
+                Debug.LogWarning("Не указана рабочая директория.");
+                WorkingDirectory = System.IO.Directory.GetCurrentDirectory();
+                //return;
+            }
+
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            int k = 0;
+
+            while (!IsCancelled)
+            {
+                Debug.Log(k++.ToString());
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            if (e.SpecialKey == ConsoleSpecialKey.ControlC)
+            {
+                IsCancelled = true;
+                e.Cancel = true;
+            }
         }
     }
 }
