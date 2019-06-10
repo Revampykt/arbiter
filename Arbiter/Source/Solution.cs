7177671fca3fe0bfd1de8599ad9ce5cce28e5385
Volodya Leveryev <volodya.leveryev@gmail.com>
2019-06-10 16:41:31 +0900

    c# invoker

---
+++ Arbiter/Source/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arbiter
+{
+    public class Solution
+    {
+        public string userName;
+        public string realName;
+        public Dictionary<string, Dictionary<string, Result>> results;
+        public Dictionary<string, string> preliminary;
+        public float total;
+        public string language;
+        public string source;
+        public bool compilation;
+        public DateTime time;
+    }
+
+    public class Result
+    {
+        public Verdict verdict;
+        public float memoryUsed;
+        public float elapsedTime;
+    }
+}
---
+++ CSharpInvoker/Program.cs
@@ -0,0 +1,80 @@
+﻿using Arbiter;
+using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace CSharpInvoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                return;
+
+            string input = File.ReadAllText(args[0]);
+
+            var deserializer = new DeserializerBuilder().WithNamingConvention(new CamelCaseNamingConvention()).Build();
+            var solution = deserializer.Deserialize<Solution>(input);
+
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                GenerateExecutable = true
+            };
+
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, solution.source);
+            if (results.Errors.HasErrors)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (CompilerError error in results.Errors)
+                {
+                    sb.AppendLine(String.Format("Error ({0}): {1}", error.ErrorNumber, error.ErrorText));
+                }
+
+                throw new InvalidOperationException(sb.ToString());
+            }
+
+            Assembly assembly = results.CompiledAssembly;
+
+            Type program = assembly.GetType("Program");
+
+            MethodInfo main = program.GetMethod("Main");
+
+            var ms = program.GetMethods();
+            foreach (var m in ms)
+            {
+                Console.WriteLine(m.Name);
+                var ps = m.GetParameters();
+                foreach (var p in ps)
+                    Console.WriteLine(p.ParameterType.Name);
+            }
+
+            var instance = assembly.CreateInstance("Program");
+
+            var type = instance.GetType();
+
+            var watch = Stopwatch.StartNew();
+
+            long beforeMemory = GC.GetTotalMemory(false);
+
+            main.Invoke(instance, new object[] { args });
+
+            long afterMemory = GC.GetTotalMemory(false);
+
+            watch.Stop();
+
+            long usedMemory = afterMemory - beforeMemory;
+
+            Console.WriteLine("UsedMemory = " + usedMemory + " ElapsedTime = " + watch.ElapsedMilliseconds);
+        }
+    }
+}
